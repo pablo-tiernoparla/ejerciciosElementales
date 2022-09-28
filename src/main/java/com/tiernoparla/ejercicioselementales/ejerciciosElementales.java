4dad79b9d395578fa26e48b524bc26d511ef1073
pablo-tiernoparla <pablo.delgadosoto@tiernoparla.es>
2022-09-28 18:08:21 +0200

    del 1 al 10 pares while

--- src/main/java/com/tiernoparla/ejercicioselementales/ejerciciosElementales.java
+++ src/main/java/com/tiernoparla/ejercicioselementales/ejerciciosElementales.java
@@ -4,13 +4,24 @@ public class ejerciciosElementales {
 
     public static void main(String[] args) {
         
+        int i = 0;
+        while(i < 11){
+            i++;
+            if(i%2 == 0){
+                System.out.println(i);
+            
+            }// if
+        }// while
+        
+        
+        /*
         //printear del 1 al 10 pares v2
         for(int i = 1; i < 11; i++){
             if(i%2 == 0){
                 System.out.println(i);
             }// if
         }// for
-
+        */
         /*
         // printear numeros pares 1 al 10
         for(int i = 2; i < 11; i = i + 2) {
