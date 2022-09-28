425789d066e796ff2cf0dd49bd7b3186b1600e49
pablo-tiernoparla <pablo.delgadosoto@tiernoparla.es>
2022-09-28 17:52:03 +0200

    print 1 a 10 pares v2

--- src/main/java/com/tiernoparla/ejercicioselementales/ejerciciosElementales.java
+++ src/main/java/com/tiernoparla/ejercicioselementales/ejerciciosElementales.java
@@ -1,14 +1,22 @@
 package com.tiernoparla.ejercicioselementales;
 
 public class ejerciciosElementales {
+
     public static void main(String[] args) {
         
+        //printear del 1 al 10 pares v2
+        for(int i = 1; i < 11; i++){
+            if(i%2 == 0){
+                System.out.println(i);
+            }// if
+        }// for
+
+        /*
         // printear numeros pares 1 al 10
-        for(int i = 2; i < 11; i = i+2){
+        for(int i = 2; i < 11; i = i + 2) {
             System.out.println(i);
         } // for
-        
-        
-       
+        */
+
     } // main
 }// ejerciciosElementales
