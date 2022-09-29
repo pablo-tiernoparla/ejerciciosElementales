20b5f038bce704ace564618020e2479a71393579
pablo-tiernoparla <pablo.delgadosoto@tiernoparla.es>
2022-09-29 18:05:52 +0200

    algoritmo de division

--- src/main/java/com/tiernoparla/ejercicioselementales/ejerciciosElementales.java
+++ src/main/java/com/tiernoparla/ejercicioselementales/ejerciciosElementales.java
@@ -1,18 +1,39 @@
 package com.tiernoparla.ejercicioselementales;
 
+import java.util.Scanner;
+
 public class ejerciciosElementales {
 
     public static void main(String[] args) {
         
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println("Dame un dividendo entero");
+        n = sc.nextInt();
+        int d;
+        System.out.println("Dame un divisor entero");
+        d = sc.nextInt();
+        
+        int r = n;
+        int q = 0;
+        while (r >= d){
+            r = r-d;
+            q++;
+        }//while    
+        System.out.println("N="+n+" D="+d+" R="+r+" Q="+q);
+        System.out.print("N=d*q+r=");
+        System.out.println((d*q)+r);
+        
+        
+        /*
         int i = 0;
         while(i < 11){
             i++;
             if(i%2 == 0){
                 System.out.println(i);
-            
             }// if
         }// while
-        
+        */
         
         /*
         //printear del 1 al 10 pares v2
